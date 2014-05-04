0871808c98e12970f56ad982b850aee4f802f8af
Erich Welz <erichwelz@gmail.com>
2014-05-04 08:33:59 -0400

    adding jwplayer Rails way?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
 //= require scriptcam
 
     /* JWPlayer cloud hosted with erichwelz@gmail.com account */
-// <script src="http://jwpsrv.com/library/KO36etOEEeO1PiIACmOLpg.js"></script>
+<src="http://jwpsrv.com/library/KO36etOEEeO1PiIACmOLpg.js">
 
 $(function() {
 	console.log("page loaded");
