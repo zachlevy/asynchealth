da9948634d964c3907ed1f61821690ba9353a481
Zach Levy <zacharyalevy@gmail.com>
2014-05-04 11:07:31 -0400

    Form integration

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,16 +1,31 @@
 class VideosController < ApplicationController
+
   def index
+    @videos = Video.all
   end
 
   def show
   end
 
   def create
+    @video = Video.new(video_params)
+    if @video.save
+      redirect_to video_path(@video)
+    else
+      render :new
+    end
   end
 
   def new
+    @video = Video.new
   end
 
   def edit
   end
+
+  private
+  def video_params
+    params.require(:video).permit(:question_id, :councillor_id, :video_url)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140504002603) do
+ActiveRecord::Schema.define(version: 20140504144650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140504002603) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "video_id"
+    t.integer  "councillor_id"
   end
 
 end
