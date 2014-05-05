9d90930d6b587d96caefffd125cd7b2629981130
Zach Levy <zacharyalevy@gmail.com>
2014-05-05 15:10:03 -0400

    changed the model to accept array (only works in postgresql)

--- db/migrate/20140504001906_create_videos.rb
+++ db/migrate/20140504001906_create_videos.rb
@@ -1,10 +1,9 @@
 class CreateVideos < ActiveRecord::Migration
   def change
     create_table :videos do |t|
-      t.string :video_url
+      t.string :video_url, array: true, default: []
       t.integer :question_id
       t.integer :timeline_id
-
       t.timestamps
     end
   end
